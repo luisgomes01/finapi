33f69636fa9903bffbbec4195accdbcb05deda2a
Luís Gomes <luis.gomes@liferay.com>
2022-03-14 09:45:07 -0300

    Register an account

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  req.send(console.log("Hello Luís!"));
+customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    id,
+    name,
+    statement: [],
+  });
+
+  return res.status(201).send();
 });
 
 app.listen(3001);
